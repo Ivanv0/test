5b253610512c6a2dfb149a186ac6c0ed38c14b2e
Ivan <Ivanvo.vk@yandex.ru>
2021-10-19 13:58:11 +0300

    Create 3-classes.py

---
+++ python/3-classes.py
@@ -0,0 +1,59 @@
+class Engine:
+    def __init__(self):
+        self.engine_start_up = False
+    def start_engine(self):
+        if self.engine_start_up:
+            print('Engine already running')
+        else:
+            print('The engine is running now')
+            self.engine_start_up = True
+    def stop_engine(self):
+        if not self.runnung:
+            print('Engine already stopped')
+        else:
+            print('The engine is stopped now')
+            self.engine_start_up = False
+
+class Pedals(Engine):
+    def __init__(self):
+        super().__init__()
+        self.gas_pedal = False
+        self.brake_pedal = False
+    def accelerate(self):
+        if self.engine_start_up:
+            print('Accelerate')
+            self.gas_pedal = True
+            self.brake_pedal = False
+        else:
+            print('You need to start_engine first')
+    def stop(self):
+        self.gas_pedal = False
+        self.brake_pedal = True
+        print('Stop')
+
+class Car(Pedals):
+    'My fantasy is over'
+    def __init__(self):
+        super().__init__()
+
+methods_list=['methods']
+def methods(ml=methods_list):
+    print()
+    print(*ml,sep=', ',end='\n\n')
+
+car = Car()
+
+print('\nSorry I was too lazy so here are the available methods.')
+for i in dir(car):
+    if i[0] != '_':
+        methods_list.append(i)
+methods()
+print('Your Car named "car"\n')
+
+while True:
+    try:
+        exec(input())
+    except KeyboardInterrupt:
+        break
+    except:
+        print('Error')
